8605beeea84171342061d8f12fbc8d34bd911d01
Trevor Dunlap <tjdunlap1899@gmail.com>
2023-02-07 10:58:57 -0500

    commit tree f4b0f936ec0c41ee7abb87cebeca621ca856eb8d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+}
+
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+}
+
+matchName(drivers, 'Bobby');
